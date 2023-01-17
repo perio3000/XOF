6eb32de01bff88112756daead9ee2e6897529c64
John <1996tjrgns@naver.com>
2023-01-17 20:52:17 +0900

    세큐리티 의존성 추가 테스트로 index.jsp 를 표시하려했으나 실패

---
+++ xof/src/main/java/com/example/xof/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.xof.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        log.info("index().......");
+        return "index";
+    }
+}
