fe433df464c1fee2afac93df8f1eaa4d26587fec
perio3000 <75698724+perio3000@users.noreply.github.com>
2023-01-17 16:59:04 +0900

    Add files via upload
    프로젝트생성

---
+++ xof/src/main/java/com/example/xof/XofApplication.java
@@ -0,0 +1,13 @@
+package com.example.xof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XofApplication.class, args);
+    }
+
+}
---
+++ xof/src/test/java/com/example/xof/XofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
